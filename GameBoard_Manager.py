7452fd8f5827cf5c1ec0e268626d027280a8ba28
Medow3 <59157960+Medow3@users.noreply.github.com>
2020-01-09 21:54:14 -0800

    Update GameBoard_Manager.py

--- GameBoard_Manager.py
+++ GameBoard_Manager.py
@@ -9,18 +9,19 @@ def drop_rows(game_array: list, player: Player, new_row="FIXME"):
         new_game_array[index + 1] = row1
         index += 1
     if new_row == "FIXME":
-        print("no row passed in")
-        new_game_array[0] = [0, 0, 0, 0, 0, 0, 0]
+        # print("no row passed in")
+        new_game_array[0] = ['.', '.', '.', '.', '.', '.', '.']
     elif new_row is None:
-        print("None row passed in")
-        new_game_array[0] = [0, 0, 0, 0, 0, 0, 0]
+        # print("None row passed in")
+        new_game_array[0] = ['.', '.', '.', '.', '.', '.', '.']
     else:
-        print("passed in row:", new_row)
+        # print("passed in row:", new_row)
         new_game_array[0] = new_row
     temp = new_game_array[len(new_game_array) - 1]
-    print("len:", len(temp))
-    print("pos:", player.position)
-    temp[player.position] = "p"
-    print("**" * 100)
-    print("leaving drop_rows")
+    # print("len:", len(temp))
+    # print("pos:", player.position)
+    player.hit_detection(temp)
+    temp[player.position] = "P"
+    # print("**" * 100)
+    # print("leaving drop_rows")
     return new_game_array
